e44d0d46bbb48450dd0cf7ce4c60a10011d799c2
Tim Hall <tim.hall.engr@gmail.com>
2019-05-24 12:23:08 -0400

    Implement useModel

--- src/use-model.ts
+++ src/use-model.ts
@@ -1,3 +1,25 @@
-export default function useModel() {
-  // TODO
+import * as Backbone from 'backbone';
+import * as React from 'react';
+import { assign, pick } from './utils';
+
+export default function useModel(model: Backbone.Model, keys?: string[]): any {
+  const select = () => {
+    return assign({}, keys ? pick(model.attributes, keys) : model.attributes);
+  };
+
+  const [attributes, setAttributes] = React.useState(select());
+  const event = keys ? keys.map(key => `change:${key}`).join(' ') : 'change';
+
+  React.useEffect(() => {
+    const onChange = () => {
+      setAttributes(select());
+    };
+    model.on(event, onChange);
+
+    return () => {
+      model.off(event, onChange);
+    };
+  }, [model]);
+
+  return attributes;
 }
---
+++ src/utils.ts
@@ -0,0 +1,20 @@
+export function assign(object: any, ...sources: any[]): any {
+  for (const source of sources) {
+    for (const key of Object.keys(source)) {
+      object[key] = source[key];
+    }
+  }
+
+  return object;
+}
+
+export function pick(object: any, keys: string[]): any {
+  const picked: any = {};
+  for (const key of Object.keys(object)) {
+    if (keys.includes(key)) {
+      picked[key] = object[key];
+    }
+  }
+
+  return picked;
+}
